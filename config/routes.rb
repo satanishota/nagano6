a5072b2a4b4a08afa170a5434d99f9fe39354f35
EC2 Default User <ec2-user@ip-172-31-45-238.ec2.internal>
2021-10-23 11:06:01 +0000

    third

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :admins 
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admin, :controllers => {
+ :registrations => 'admin/registrations',
+ :sessions => 'admin/sessions'}
 
 
 Rails.application.routes.draw do
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+  end
   scope module: :public do
-    devise_for :customers
+    devise_for :customers, :controllers => {
+ :registrations => 'public/registrations',
+ :sessions => 'public/sessions'}
+ resources :customers, only: [:edit, :show, :update]
+
   end
 end
 end
