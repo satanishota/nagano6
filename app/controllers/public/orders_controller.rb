92459b76053fbf93363fa5fd20261a4e664600ea
satanishota <figuree5252@gmail.com>
2021-10-31 14:41:09 +0000

    14th

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -14,18 +14,8 @@ class Public::OrdersController < ApplicationController
 
   def create
     @order = current_customer.orders.new(order_params)
-    if @order.invalid?
-      redirect_to new_order_path
-    else
-      render :confirm
-    end
 
-
-  end
-
-  def confirm
-      @order = current_customer.orders.new(order_params)
-   @order.save
+      @order.save
    @cart_items = current_customer.cart_items.all
      @cart_items.each do |cart_item|
         @order_details = @order.order_details.new
@@ -34,10 +24,35 @@ class Public::OrdersController < ApplicationController
         @order_details.purchased_price = cart_item.item.price
         @order_details.save
 
-     end
-      render :complete
+    end
 
 
+  end
+
+  def confirm
+      @order = current_customer.orders.new
+      @order.payment_method = params[:order][:payment_method]
+      @order.shipping_fees = params[:order][:shipping_fees]
+      @order.customer_id = params[:order][:customer_id]
+
+      if params[:order][:select_address] == 0
+        @order.postal_code = current_customer.postal_code
+        @order.address = current_customer.address
+        @order.name = current_customer.first_name + current_customer.last_name
+      elsif params[:order][:select_address] == 1
+        @address = Address.find(params[:order][:address_id])
+        @order.postal_code = @address.postal_code
+        @order.address = @address.address
+        @order.name = @address.name
+
+      elsif params[:order][:select_address] == 2
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+
+      end
+      @cart_items = current_customer.cart_items
+  render :confirm
   end
 
   def complete
@@ -45,8 +60,7 @@ class Public::OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :shipping_fees,
-    :billing_amount, :payment_method)
+    params.require(:order).permit(:shipping_fees,:billing_amount, :payment_method)
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,9 +3,7 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
 
-  validates :postal_code, presence: true
-  validates :address, presence: true
-  validates :name, presence: true
+
   validates :payment_method, presence: true
-  
+
 end
